18cb164ef525f740d242205c7585ebb297bf6d91
MdNoor300 <sehzad.noor98@gmail.com>
2025-03-03 16:30:43 +0600

    making custom library

---
+++ CustomReact/customreact.js
@@ -0,0 +1,20 @@
+function mainContainer(reactElement, container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('herf', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+    container.appendChild(domElement)
+    container.appendChild(domElement)
+}
+const reactElement = {
+    type:'a',
+    props:{
+        href:'https://www.google.com',
+        target:'_blank'
+    },
+    children:'Click me to visit Google'
+}
+
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement, mainContainer)
