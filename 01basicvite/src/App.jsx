31f63aa14bd9006498150a1ca6132b080a85cdff
MdNoor300 <sehzad.noor98@gmail.com>
2025-03-06 16:33:44 +0600

    some modifications

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,9 +1,8 @@
 import Youtube from "./Youtube"
 function App() {
-
   return (
     <>
-          <h1>My Vite React App</h1>
+          <h1>My Vite React App </h1>
           <h1>My Vite React App</h1>
           <Youtube/>
     </>
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,9 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).
+const reactElement = {
+    type:'a',
+    props:{
+        href:'https://www.google.com',
+        target:'_blank'
+    },
+    children:'Click me to visit Google'
+}
+
+function My(){
+    return (
+        <h1>Hello bro</h1>
+    )
+}
+
+const AnotherElement = (
+    <a href="https://www.google.com" target="_blank">visit Google</a>
+)
+
+const Areact = React.createElement(
+    'a',
+    {
+        href:'https://www.google.com',
+        target:'_blank'
+    },
+    'Click me to visit Google'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).
 render(
-    <App />,
-    <youtube/>
+    <App/>
+
 )
