89edb6a84b2df265fde956b6da432db3f986614e
MdNoor300 <sehzad.noor98@gmail.com>
2025-03-08 12:11:37 +0600

    tailwind and props section done

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -7,10 +7,13 @@ import Card from './component/Card'
 function App() {
   const [count, setCount] = useState(0)
 
+   let newArr = [1,2,3,4,5,6,7,8,9]
   return (
     <>
      <h1 className='text-3xl bg-green-500 rounded-md p-3 ' >Vite with Tailwind</h1>
      <label htmlFor=""></label>
+     <Card username="noor" myArr = {newArr}/>
+     <Card username='Sohag' post='Good'/>
      <Card />
 
     </>
--- 03tailwindprops/src/component/Card.jsx
+++ 03tailwindprops/src/component/Card.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({username = "santo" , post = "Good Boy", myArr}) => {
+  //console.log(props);
+  
   return (
-    <div class="flex items-center bg-gray-300 dark:bg-gray-800 mt-2 rounded-md">
-      <div class="flex flex-col items-center gap-6 md:flex-row md:gap-8 rounded-2xl">
+    <div className="flex items-center bg-gray-300 dark:bg-gray-800  rounded-md">
+      <div className="flex flex-col items-center gap-6 md:flex-row md:gap-8 rounded-2xl">
         <div>
           <img
-            class="size-48 shadow-xl rounded-l"
+            className="size-48 shadow-xl rounded-l"
             alt=""
             src="https://img.freepik.com/free-photo/landscape-morning-fog-mountains-with-hot-air-balloons-sunrise_335224-794.jpg?uid=R32176361&ga=GA1.1.2028958094.1725276662&semt=ais_hybrid"
           />
         </div>
         <div class="flex items-center md:items-start">
-          <span class="text-2xl font-medium">Class Warfare</span>
-          <span class="font-medium text-sky-500 p-2">The Anti-Patterns</span>
-          <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-            <span class = "p-2">No. 4</span>
-            <span class = "p-2">·</span>
-            <span class = "p-2">2025</span>
+          <span className="text-2xl font-medium">{username}</span>
+          <span className="font-medium text-sky-500 p-2">{post}</span>
+          <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+            <span className = "p-2">{myArr}</span>
+            <span className = "p-2">·</span>
+            <span className = "p-2">2025</span>
           </span>
         </div>
       </div>
