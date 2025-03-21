56daac552a9b7498a17077583c906cfe1c7c714c
MdNoor300 <sehzad.noor98@gmail.com>
2025-03-21 16:42:27 +0600

    working on todo item

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {TodoProvider} from './contexts'
 import './App.css'
 
@@ -20,6 +20,20 @@ const deleteTodo = (id) => {
 const toggleCompleted = (id) => {
   setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo)))
 }
+
+
+useEffect(() => {
+  const todos = JSON.parse(localStorage.getItem("todos"))
+  if (todos && todos.length > 0) {
+    setTodos(todos)
+  }
+}, [])
+
+useEffect(() => {
+  localStorage.setItem("todos", JSON.stringify(todos))
+}, [todos])
+
+
 
   return (
     <TodoProvider
